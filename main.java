59c6aad76e44e6cede54c5a4316b780b860194ae
Jose Molina <mol14492@uvg.edu.gt>
2016-10-02 10:03:00 -0600

    Update main.java

--- main.java
+++ main.java
@@ -4,13 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		Paciente temp = new Paciente();
-		String nombret, nombre;
-		
-	    String texto1=temp.leerArchivo("texto.txt");
-	    String texto=texto1.substring(0, texto1.length()-1);
-	    System.out.println("Paciente: \n"+texto);
+
 	}
 
 }
