d12fc92d7ad2d02346a127c351edeed6da4c9d88
bau192 <bau15192@uvg.edu.gt>
2016-10-02 21:12:30 -0600

    finalizado

--- main.java
+++ main.java
@@ -7,25 +7,37 @@ public class main {
 	private static String texto,texto1;
 	private static VectorHeap<String> vec;
 	
+	
 	public static void main (String[] args) {
-		// TODO Auto-generated method stub
-		
+	
+try	{	// TODO Auto-generated method stub
+		String [] personas=new String [100];
 		// Imprime el titulo.
-		System.out.println("Systema de Pacientes.\n");
-		
+		System.out.println("\nSystema de Pacientes.\n");
 		// Se inicializan las variable.
 		 file = new Paciente();
 		 texto = file.leerArchivo("paciente.txt");
 		//System.out.println(texto);
 		texto1="";
 		 vec = new VectorHeap<String>();
-		 
 		 // Ingreso de los pacientes 
 		for (String texto1:texto.split("\n")){
 			String texto3 = texto1.substring(texto1.length()-1);
-			vec.add(texto3);			
-		}	
+			vec.add(texto3);				
+			}	
 		
+		
+		System.out.println("| Prioridad  | Nombre y enfermedad/Herida      	|");
+		while(!vec.isEmpty())
+		{
+			System.out.println("    " + vec.removeFirst());
+			
+		}
+		
+	}catch (Exception e){ //Catch de excepciones
+            System.err.println("Ocurrio un error:\n    ->No se encontro el archivo\n " + e.getMessage());
+        }
+	
 	}
 
 }
