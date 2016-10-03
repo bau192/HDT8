32ed790c2339ffe04ad8e1355740402a6c7cfb46
Jose Molina <mol14492@uvg.edu.gt>
2016-10-02 18:41:56 -0600

    Update main.java

--- main.java
+++ main.java
@@ -1,10 +1,30 @@
 import java.util.*;
+import java.io.*;
 
 public class main {
-
-	public static void main(String[] args) {
+		
+	private static Paciente file;
+	private static String texto,texto1;
+	private static VectorHeap<String> vec;
+	
+	public static void main (String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		// Imprime el titulo.
+		System.out.println("Systema de Pacientes.\n");
+		
+		// Se inicializan las variable.
+		 file = new Paciente();
+		 texto = file.leerArchivo("paciente.txt");
+		 texto1="";
+		 vec = new VectorHeap<String>();
+		 
+		 // Ingreso de los pacientes 
+		for (String texto1:texto.split("\n")){
+			String texto3 = texto1.substring(texto1.length()-1);
+			vec.add(texto3);			
+		}	
+		
 	}
 
 }
